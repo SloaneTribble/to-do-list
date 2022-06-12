67b1e656f9b5bd5d08c082815bbe3caf2af96165
Sloane Tribble <SloaneTribble@gmail.com>
2022-06-12 15:32:14 -0700

    Basic set up

---
+++ dist/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("YOM");
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
