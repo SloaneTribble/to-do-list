fad65a88852ca1e998da6876d8df6db074a5492f
Sloane Tribble <SloaneTribble@gmail.com>
2022-06-12 16:49:18 -0700

    Test for proper use of setters and getters between modules

--- bundle.js
+++ bundle.js
@@ -75,6 +75,27 @@ class Project{
 
 /***/ }),
 
+/***/ "./src/test.js":
+/*!*********************!*\
+  !*** ./src/test.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "test": () => (/* binding */ test)
+/* harmony export */ });
+// Testing to make sure that functions defined insidem modules can use 
+// setters and getters from other modules
+
+function test(input){
+    return input.getToDos();
+}
+
+
+
+/***/ }),
+
 /***/ "./src/todo.js":
 /*!*********************!*\
   !*** ./src/todo.js ***!
@@ -202,6 +223,8 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _project__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./project */ "./src/project.js");
 /* harmony import */ var _todo__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./todo */ "./src/todo.js");
+/* harmony import */ var _test__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./test */ "./src/test.js");
+
 
 
 
@@ -214,6 +237,8 @@ newProject.addToDo(newToDo);
 newProject.addToDo("To do 2");
 
 newProject.getToDos();
+
+(0,_test__WEBPACK_IMPORTED_MODULE_2__.test)(newProject);
 
 console.log("Yes");
 })();
